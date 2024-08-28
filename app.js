9a5b4a3f1826df9376d454e806ec0aea6a98815c
MASHRAFI47 <mbngms@gmail.com>
2024-08-28 07:50:32 +0600

    get one meal added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iduz7rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -61,6 +61,13 @@ async function run() {
 
         app.get("/meals", async (req, res) => {
             const result = await mealsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/meals/:id", async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await mealsCollection.findOne(query);
             res.send(result);
         })
 
