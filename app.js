f07bd415b92d05941164354cc3387ec27df69283
MASHRAFI47 <mbngms@gmail.com>
2024-08-29 13:01:37 +0600

    jwt api added

--- app.js
+++ app.js
@@ -146,6 +146,19 @@ async function run() {
             res.send(result);
         })
 
+        //jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d'
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true })
+        })
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
