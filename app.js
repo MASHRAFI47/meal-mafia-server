ee8e164968abf1fcd010d452c838fd3db9ff891c
MASHRAFI47 <mbngms@gmail.com>
2024-08-30 22:25:54 +0600

    user role api added

--- app.js
+++ app.js
@@ -149,6 +149,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/user/role/:email", async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email}
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+        })
+
         //jwt
         app.post('/jwt', async (req, res) => {
             const user = req.body;
