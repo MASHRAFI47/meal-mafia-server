414bdb8e9cc121ebc1403b31c1587d7490ffb6a9
MASHRAFI47 <mbngms@gmail.com>
2024-09-01 13:05:34 +0600

    meal delete api added

--- app.js
+++ app.js
@@ -128,6 +128,14 @@ async function run() {
             res.send(result);
         })
 
+        //delete a meal
+        app.delete("/meal/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await mealsCollection.deleteOne(query);
+            res.send(result);
+        })
+
         //get all users
         app.get("/users", async (req, res) => {
             const result = await usersCollection.find().toArray();
